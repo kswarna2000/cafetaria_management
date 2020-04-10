6a4788b903fa7cb02caa007b5c1b1c8ce206eaaa
kswarna2000 <kswarna2000@gmail.com>
2020-04-11 05:18:51 +0530

    Added Place order functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,16 @@ class OrdersController < ApplicationController
   def index
     render "index"
   end
+
+  def create
+    order_date = Date.today
+    user_id = 1
+    delivered_at = nil
+    new_order = Order.create!(
+      order_date: order_date,
+      user_id: user_id,
+      delivered_at: delivered_at,
+    )
+    redirect_to orderitems_path
+  end
 end
