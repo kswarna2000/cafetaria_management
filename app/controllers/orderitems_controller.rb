358af58bc00fb634b1e0741b09b12f407f90310e
kswarna2000 <kswarna2000@gmail.com>
2020-04-11 06:43:20 +0530

    Added item order feature to the application

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -2,4 +2,22 @@ class OrderitemsController < ApplicationController
   def index
     render "index"
   end
+
+  def create
+    order_id = params[:order_id]
+    menu_item_id = params[:menu_item_id]
+    menu_item_name = params[:menu_item_name]
+    menu_item_price = params[:menu_item_price]
+    neworderitem = Orderitem.create!(
+      order_id: order_id,
+      menu_item_id: menu_item_id,
+      menu_item_name: menu_item_name,
+      menu_item_price: menu_item_price,
+    )
+    redirect_to orderitems_path
+  end
+
+  def redirect
+    redirect_to orders_path
+  end
 end
