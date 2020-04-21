582a0dbc317b2d482bb2a7a41da0392600702492
kswarna2000 <kswarna2000@gmail.com>
2020-04-21 23:31:00 +0530

    Added edit and delete item functionality

--- app/controllers/menuitems_controller.rb
+++ app/controllers/menuitems_controller.rb
@@ -17,4 +17,26 @@ class MenuitemsController < ApplicationController
     )
     redirect_to new_menuitem_path
   end
+
+  def edit
+    @update_menuitem = Menuitem.find(params[:id])
+    render "edititem"
+  end
+
+  def destroy
+    id = params[:id]
+    menuitem = Menuitem.find(id)
+    menuitem.destroy
+    redirect_to new_menuitem_path
+  end
+
+  def update
+    id = params[:id]
+    menuitem = Menuitem.find(id)
+    menuitem.item_name = params[:item_name]
+    menuitem.item_description = params[:item_description]
+    menuitem.item_price = params[:item_price]
+    menuitem.save
+    redirect_to new_menuitem_path
+  end
 end
