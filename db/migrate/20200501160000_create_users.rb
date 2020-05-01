0f435942d18d10842177bb9102fa7e94cc01b2d2
kswarna2000 <kswarna2000@gmail.com>
2020-05-01 21:35:55 +0530

    Added a migration

--- db/migrate/20200501160000_create_users.rb
+++ db/migrate/20200501160000_create_users.rb
@@ -1,13 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      create_table :users do |t|
-        t.string :first_name
-        t.string :last_name
-        t.string :email
-        t.string :password_digest
-        t.timestamps
-        t.string :role
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.timestamps
+      t.string :role
     end
   end
 end
