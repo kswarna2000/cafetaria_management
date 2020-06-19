2d72f8523bdc7fce5039760b1134fb9e1ede9539
kswarna2000 <kswarna2000@gmail.com>
2020-06-19 23:10:47 +0530

    Small code modifications

--- app/models/Orderitem.rb
+++ app/models/Orderitem.rb
@@ -2,4 +2,7 @@ require "active_record"
 
 class Orderitem < ActiveRecord::Base
   validates :menu_item_quantity, numericality: { greater_than: 0 }
+  def self.in_cart
+    where(in_cart: true)
+  end
 end
